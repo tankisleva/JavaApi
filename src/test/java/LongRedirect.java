cb7ab0577812ac58d0abc4c3f820de094c5846de
Malyshev Oleg <malyshev@jivosite.com>
2024-06-19 23:15:30 +0300

    long redirect

---
+++ src/test/java/LongRedirect.java
@@ -0,0 +1,33 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class LongRedirect {
+
+    @Test
+    public void testFollowRedirects() {
+        String initialUrl = "https://playground.learnqa.ru/api/long_redirect";
+        String redirectUrl = initialUrl;
+        int statusCode;
+
+        // Цикл, который будет выполнять запросы, пока не получит статус 200
+        do {
+            Response response = RestAssured
+                    .given()
+                    .redirects().follow(false) // Не следуем за редиректами автоматически
+                    .get(redirectUrl);
+
+            statusCode = response.getStatusCode();
+
+            if (statusCode != 200) {
+                redirectUrl = response.getHeader("Location");
+                System.out.println("Redirecting to: " + redirectUrl);
+            } else {
+                System.out.println("Final URL: " + redirectUrl);
+            }
+        } while (statusCode != 200);
+
+        // Дополнительно можно добавить проверку, что статус код действительно 200
+        assert(statusCode == 200);
+    }
+}
