d0cb2ded6c9d3423c8fe91a89b120903e3df5dcb
Malyshev Oleg <malyshev@jivosite.com>
2024-06-20 21:11:37 +0300

    testStringLength

---
+++ src/test/java/StringLengthTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringLengthTest {
+
+    @Test
+    public void testStringLength() {
+        String text = "Some text longer than 15 characters";
+        assertTrue(text.length()>15,"Length is not greater than 15");
+    }
+}
