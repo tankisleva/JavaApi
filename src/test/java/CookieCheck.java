fe2ad8e86a66c8fa5295c0ee70742f29c62c34c4
Malyshev Oleg <malyshev@jivosite.com>
2024-06-20 21:27:53 +0300

    test cookie

---
+++ src/test/java/CookieCheck.java
@@ -0,0 +1,27 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CookieCheck {
+
+
+    @Test
+    public void testСookie(){
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured.given().body(authData).post("https://playground.learnqa.ru/api/user/login").andReturn();
+
+        Map<String, String> cookies = responseGetAuth.getCookies();
+        assertTrue(cookies.containsKey("auth_sid"), "Response doesn't have 'auth_sid' cookie");
+    }
+}
