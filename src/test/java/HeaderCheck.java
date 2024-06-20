474295b379131444bae5ef7fbd2912b940c1d171
Malyshev Oleg <malyshev@jivosite.com>
2024-06-20 21:33:15 +0300

    test header

---
+++ src/test/java/HeaderCheck.java
@@ -0,0 +1,30 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HeaderCheck {
+
+    @Test
+    public void testHeader(){
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured.given().body(authData).post("https://playground.learnqa.ru/api/user/login").andReturn();
+
+
+        Headers headers = responseGetAuth.getHeaders();
+
+
+        assertTrue(headers.hasHeaderWithName("x-csrf-token"), "Response doesn't have 'x-csrf-token' header");
+
+    }
+}
